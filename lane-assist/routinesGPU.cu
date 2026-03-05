e68e04896c702a6a1da3cf741062ac0b90f415ae
Antonino Sasu <161477399+AntoninoSasu@users.noreply.github.com>
2026-03-05 15:15:39 +0100

    ahora si lane-assist

---
+++ lane-assist/routinesGPU.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+
+#include "routinesGPU.h"
+
+void lane_assist_GPU(uint8_t *im, int height, int width,
+	int *x1, int *y1, int *x2, int *y2, int *nlines)
+{
+
+	/* To do */
+}
---
+++ lane-assist/routinesGPU.h
@@ -0,0 +1,10 @@
+#ifndef ROUTINESGPU_H
+#define ROUTINESGPU_H
+
+#include <stdint.h>
+
+void lane_assist_GPU(uint8_t *im, int height, int width,
+	int *x1, int *y1, int *x2, int *y2, int *nlines);
+
+#endif
+
