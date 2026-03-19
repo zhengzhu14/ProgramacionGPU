341994546b22247304bd7ccd3509efff38c45817
AntoninoSasu <161477399+AntoninoSasu@users.noreply.github.com>
2026-03-19 15:45:03 +0100

    Refactor lane_assist_GPU function parameters

--- lane-assist/routinesGPU.h
+++ lane-assist/routinesGPU.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 void lane_assist_GPU(uint8_t *im, int height, int width,
-	uint8_t *imEdge, float *NR, float *G, float *phi, float *Gx, float *Gy, uint8_t *pedge,
+	uint8_t *imEdge,
 	float *sin_table, float *cos_table, 
 	uint32_t *accum, int accu_height, int accu_width,
 	int *x1, int *y1, int *x2, int *y2, int *nlines);
